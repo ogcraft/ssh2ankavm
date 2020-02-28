8907ea0eb431d2fcf284156df74fc1e3b65bc6d7
Oleg Galbert <ogcraft@gmail.com>
2020-02-28 19:48:58 +0200

    Add test for check_parse_ifconfig

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "No such file or directory")]
     fn check_run_ifconfig() {
 
         let ifconfig_args = ["run", "-n", "catalina_10.15_test", "ifconfig"];
@@ -57,6 +58,43 @@ mod tests {
         let output_str = String::from_utf8(output.stdout)
                             .expect("Cannot convert output");
         let output_lines:Vec<&str>= output_str.lines().collect();
+
+        println!("output: [{:?}]", output_lines);
+
+        output_lines.iter().for_each(|l| println!("{:?}", l));
+
+        assert_eq!(0,0);
+    }
+    #[test]
+        fn check_parse_ifconfig() {
+
+        let ifconfig_otput = "lo0: flags=8049<UP,LOOPBACK,RUNNING,MULTICAST> mtu 16384
+        options=1203<RXCSUM,TXCSUM,TXSTATUS,SW_TIMESTAMP>
+        inet 127.0.0.1 netmask 0xff000000
+        inet6 ::1 prefixlen 128
+        inet6 fe80::1%lo0 prefixlen 64 scopeid 0x1
+        nd6 options=201<PERFORMNUD,DAD>
+    gif0: flags=8010<POINTOPOINT,MULTICAST> mtu 1280
+    stf0: flags=0<> mtu 1280
+    XHC7: flags=0<> mtu 0
+    en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500
+        options=423<RXCSUM,TXCSUM,TSO4,CHANNEL_IO>
+        ether 56:45:45:67:8c:df
+        inet6 fe80::808:e2d4:3d15:5b23%en0 prefixlen 64 secured scopeid 0x5
+        inet 192.168.64.24 netmask 0xffffff00 broadcast 192.168.64.255
+        nd6 options=201<PERFORMNUD,DAD>
+        media: 1000baseT <full-duplex>
+        status: active
+    utun0: flags=8051<UP,POINTOPOINT,RUNNING,MULTICAST> mtu 1380
+        inet6 fe80::418c:b5dc:c458:32c2%utun0 prefixlen 64 scopeid 0x6
+        nd6 options=201<PERFORMNUD,DAD>
+    utun1: flags=8051<UP,POINTOPOINT,RUNNING,MULTICAST> mtu 2000
+        inet6 fe80::adf2:9b24:f441:6b41%utun1 prefixlen 64 scopeid 0x7
+        nd6 options=201<PERFORMNUD,DAD>";
+        
+        let ifconfig_output_str = ifconfig_otput.to_string();
+
+        let output_lines:Vec<&str>= ifconfig_output_str.lines().collect();
 
         println!("output: [{:?}]", output_lines);
 
